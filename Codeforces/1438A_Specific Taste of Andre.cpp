b7d4fcbe85768b172f6dbd55869f1aed4cc4136c
bellicose77 <16201082@uap-bd.edu>
2020-11-20 01:00:50 +0600

    done

--- Codeforces/1438A_Specific Taste of Andre.cpp
+++ Codeforces/1438A_Specific Taste of Andre.cpp
@@ -2,6 +2,17 @@
 using  namespace std;
 int main()
 {
+    int t,n;
+    cin>>t;
+    while(t--)
+    {
+        cin>>n;
+        for(int i=1;i<=n;i++)
+        {
+            cout<<1<<" ";
+        }
+        cout<<endl;
+    }
 
     return 0;
 }
