23c84d149ea39ead6fe10a8aab20be9150ac9e13
bellicose77 <64400354+bellicose77@users.noreply.github.com>
2021-08-15 02:00:00 +0600

    279

--- Codeforces/Div 2/279B_Books.cpp
+++ Codeforces/Div 2/279B_Books.cpp
@@ -1,3 +1,4 @@
+/*
 #include<bits/stdc++.h>
 using namespace std;
 int main()
@@ -22,5 +23,78 @@ int main()
         }
     }
     cout<<c<<endl;
+    return 0;
+}
+*/
+/* Time limit exceed code
+#include<bits/stdc++.h>
+using namespace std;
+int books(vector<int>v,long long int n)
+{
+    int max_b = 0;
+    for(int i=0;i<v.size();i++)
+    {
+        int c=0,sum=0;
+        for(int j=i;j<v.size();j++)
+        {
+            sum+=v[j];
+            if(sum<=n)
+            {
+                c++;
+            }
+        }
+        max_b = max(max_b,c);
+    }
+    return max_b;
+}
+int main()
+{
+    int t,x;
+    long long int n;
+    cin>>t>>n;
+    vector<int>v;
+    while(t--)
+    {
+        cin>>x;
+        v.push_back(x);
+    }
+    int res = books(v,n);
+    cout<<res<<endl;
+    return 0;
+}
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int books(vector<int>v,long long int n)
+{
+    int sum=0,j=0,c=0;
+    for(int i=0;i<v.size();i++)
+    {
+        sum+=v[i];
+        if(sum<=n)
+        {
+            c++;
+        }
+        else
+        {
+            sum-=v[j];
+            j++;
+        }
+    }
+    return c;
+}
+int main()
+{
+    int t,x;
+    long long int n;
+    cin>>t>>n;
+    vector<int>v;
+    while(t--)
+    {
+        cin>>x;
+        v.push_back(x);
+    }
+    int res = books(v,n);
+    cout<<res<<endl;
     return 0;
 }
