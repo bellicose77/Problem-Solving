6d64d11babef7d5f0b139dda3bbc7984737f00a4
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-02-20 20:50:11 +0600

    a

--- Codeforces/Div 2/1638B_Odd Swap Sort.cpp
+++ Codeforces/Div 2/1638B_Odd Swap Sort.cpp
@@ -3,12 +3,20 @@ using namespace std;
 void swap_sort(int n){
     long long int x;
     vector<long long int>v;
+    int even=0,odd=0;
     while(n--){
         cin>>x;
+        if(x%2==0){
+            even++;
+        }
+        else{
+            odd++;
+        }
         v.push_back(x);
     }
     int flag=0;
     for(int i=0;i<v.size()-1;i++){
+
         if(v[i]>v[i+1]){
             flag=1;
             break;
@@ -17,8 +25,15 @@ void swap_sort(int n){
     if(flag==0){
         cout<<"YES"<<endl;
     }
-    else{
+    else if(even==b){
         cout<<"NO"<<endl;
+    }
+    else if(odd==b){
+        cout<<"NO"<<endl;
+    }
+    else{
+
+
     }
 }
 int main(){
