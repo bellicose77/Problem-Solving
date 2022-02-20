7c9e5430e458599217340d465353f768a00bb05d
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-02-21 00:17:03 +0600

    try something new

--- Codeforces/Div 2/1638B_Odd Swap Sort.cpp
+++ Codeforces/Div 2/1638B_Odd Swap Sort.cpp
@@ -1,3 +1,4 @@
+/*
 #include<bits/stdc++.h>
 using namespace std;
 void swap_sort(int n){
@@ -44,4 +45,59 @@ int main(){
         swap_sort(n);
     }
 return 0;
+}
+*/
+#include<bits/stdc++.h>
+using namespace std;
+void swap_pair(int n)
+{
+    int x;
+    vector<int>v;
+    while(n--){
+        cin>>x;
+        v.push_back(x);
+    }
+    vector<int>ev;
+    vector<int>od;
+
+    for(int i=0;i<v.size();i++){
+        if(v[i]%2==0){
+            ev.push_back(v[i]);
+        }
+        else{
+            od.push_back(v[i]);
+        }
+    }
+    int flag=0;
+    for(int i=0;i<ev.size();i++){
+        if(ev[i]>v[i+1]){
+            flag=1;
+            break;
+        }
+    }
+    int f=0;
+    for(int i=0;i<od.size();i++){
+        if(od[i]>od[i+1]){
+            f=1;
+            break;
+        }
+    }
+    if(flag== 0 && f==0){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+
+}
+int main()
+{
+    int t,n;
+    cin>>t;
+    while(t--)
+    {
+        cin>>n;
+        swap_pair(n);
+    }
+    return 0;
 }
