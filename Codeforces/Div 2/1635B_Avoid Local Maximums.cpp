af8ae4940355a4e80b535823ff7bc61910711077
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-02-21 16:39:44 +0600

    by idea

--- Codeforces/Div 2/1635B_Avoid Local Maximums.cpp
+++ Codeforces/Div 2/1635B_Avoid Local Maximums.cpp
@@ -1,6 +1,56 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main(){
+void local(int n)
+{
+    int b=n-2;
+    int x;
+    vector<int>v;
+    while(n--)
+    {
+        cin>>x;
+        v.push_back(x);
+    }
+    int c=0;
+    for(int i=1; i<v.size()-1; i++)
+    {
+        if(v[i]>v[i-1] && v[i]>v[i+1])
+        {
+            c++;
+            if(i!=b)
+            {
+                if(v[i]>v[i+2])
+                {
+                    v[i+1]=v[i];
+                }
+                else
+                {
+                    v[i+1]=v[i+2];
+                }
 
-return 0;
+            }
+            else{
+                     v[i+1]=v[i];
+
+            }
+
+        }
+    }
+    cout<<c<<endl;
+    for(int i=0; i<v.size(); i++)
+    {
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
+}
+int main()
+{
+    int t,n;
+    cin>>t;
+    while(t--)
+    {
+        cin>>n;
+        local(n);
+    }
+
+    return 0;
 }
