3ec3d17676495e79274a733bcb12f9b142ccd76c
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-03-08 22:07:13 +0600

    a solve

--- Codeforces/Div 3/1650A_Deletions of two Adjacent.cpp
+++ Codeforces/Div 3/1650A_Deletions of two Adjacent.cpp
@@ -1,11 +1,35 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main(){
+void sss()
+{
+    string s;
+    char c;
+    cin>>s;
+    cin>>c;
+    int flag=0;
+    for(int i=0;i<s.length();i+=2)
+    {
+       if(c==s[i]){
+        cout<<"YES"<<endl;
+        flag=1;
+        break;
+       }
+
+
+    }
+    if(flag==0)
+    {
+        cout<<"NO"<<endl;
+    }
+}
+int main()
+{
     int t;
     cin>>t;
-    while(t--){
-        cin>>s;
+    while(t--)
+    {
+        sss();
     }
 
-return 0;
+    return 0;
 }
