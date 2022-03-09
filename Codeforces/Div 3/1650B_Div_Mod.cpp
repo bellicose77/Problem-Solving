9c39671a2a05110dc2da74221959f0402fdb1933
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-03-09 23:32:04 +0600

    ki je korbo

--- Codeforces/Div 3/1650B_Div_Mod.cpp
+++ Codeforces/Div 3/1650B_Div_Mod.cpp
@@ -2,8 +2,11 @@
 using namespace std;
 void div()
 {
-    long long int l,r,a,sum,les_higer,di ;
+
+    long long int l,r,a,sum,les_higer,pr,hr,fm,lm;
+
     cin>>l>>r>>a;
+    /*
     if(a==1)
     {
         cout<<r<<endl;
@@ -29,6 +32,33 @@ void div()
 
 
     }
+    */
+   if(l==r && l==a){
+        cout<<1<<endl;
+    }
+    else if(r<a){
+        cout<<r<<endl;
+    }
+    else if(a<l){
+        long long int xx = r/a + (r%a);
+        long long int yy = ((r-1)/a)+((r-1)%a);
+        cout<<max(xx,yy)<<endl;
+    }
+    else if(r>=a && a>=l){
+
+        sum = (r/a)+(r%a);
+        pr = a-1;
+        les_higer = r-1;
+        hr = (les_higer/a)+(les_higer%a);
+        fm = max(sum,pr);
+        lm = max(fm,hr);
+        cout<<lm<<endl;
+    }
+
+    else{
+        cout<<r<<endl;
+    }
+
 
 }
 int main()
