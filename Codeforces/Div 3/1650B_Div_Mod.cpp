55be0d7d0d04825c9fcfcd5390d27b92111ed766
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-03-10 01:13:57 +0600

    done

--- Codeforces/Div 3/1650B_Div_Mod.cpp
+++ Codeforces/Div 3/1650B_Div_Mod.cpp
@@ -3,62 +3,19 @@ using namespace std;
 void div()
 {
 
-    long long int l,r,a,sum,les_higer,pr,hr,fm,lm;
+    long long int l,r,a,q,q1,re;
 
     cin>>l>>r>>a;
-    /*
-    if(a==1)
-    {
-        cout<<r<<endl;
+    q=l/a;
+    q1=r/a;
+    if(q==q1){
+        re=q+(r%a);
+        cout<<re<<endl;
     }
-    else if(l==r && l==a){
-        cout<<1<<endl;
-    }
-    else
-    {
-        if(r%a!=0)
-        {
-            di= r/a;
-            sum=di+(r%a);
-            cout<<sum<<endl;
-
-        }
-        else{
-            les_higer = r-1;
-            di = les_higer/a;
-            sum = di+(les_higer%a);
-            cout<<sum<<endl;
-        }
-
-
-    }
-    */
-   if(l==r && l==a){
-        cout<<1<<endl;
-    }
-    else if(r<a){
-        cout<<r<<endl;
-    }
-    else if(a<l){
-        long long int xx = r/a + (r%a);
-        long long int yy = ((r-1)/a)+((r-1)%a);
-        cout<<max(xx,yy)<<endl;
-    }
-    else if(r>=a && a>=l){
-
-        sum = (r/a)+(r%a);
-        pr = a-1;
-        les_higer = r-1;
-        hr = (les_higer/a)+(les_higer%a);
-        fm = max(sum,pr);
-        lm = max(fm,hr);
-        cout<<lm<<endl;
-    }
-
     else{
-        cout<<r<<endl;
+        re=max(((r/a)+(r%a)),((r/a)-1+a-1));
+        cout<<re<<endl;
     }
-
 
 }
 int main()
