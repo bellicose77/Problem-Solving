eb12d5ecc9ecfebd4d4d1bc25a813d28abdabbf4
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-03-11 14:48:35 +0600

    done

--- Codeforces/Div 2/1651B_Prove him wrong.cpp
+++ Codeforces/Div 2/1651B_Prove him wrong.cpp
@@ -1,6 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 void wrong(int n){
+    int x;
+    vector<int>v;
+    v.push_back(1);
+    //cout<<v[0]<<endl;
+    for(int i=0;i<n-1;i++){
+        x=v[i]*3;
+        if(x<1000000000){
+            v.push_back(x);
+        }
+    }
+    if(v.size()==n){
+        cout<<"YES"<<endl;
+        for(int i=0;i<v.size();i++){
+            cout<<v[i]<<" ";
+        }
+        cout<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+
 
 }
 int main(){
