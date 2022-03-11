a740134ebdda337e05169acdaa9d43eda59f9c25
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-03-12 00:59:40 +0600

    done

--- Codeforces/Div 2/1647A_Madoka and Math Dad.cpp
+++ Codeforces/Div 2/1647A_Madoka and Math Dad.cpp
@@ -2,7 +2,50 @@
 using namespace std;
 void digit(int n)
 {
+    int c=1;
+    if(n%3==0 || n%3==2)
+    {
 
+        while(n!=0)
+        {
+            if(c%2==1)
+            {
+                cout<<2;
+                n-=2;
+                c++;
+            }
+            else
+            {
+                cout<<1;
+                n-=1;
+                c++;
+            }
+        }
+        cout<<endl;
+
+    }
+    else if(n%3==1)
+    {
+        while(n!=0)
+        {
+            if(c%2==1)
+            {
+                cout<<1;
+                n-=1;
+                c++;
+
+            }
+            else{
+                cout<<2;
+                n-=2;
+                c++;
+            }
+
+        }
+        cout<<endl;
+
+
+    }
 
 }
 
