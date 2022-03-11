0432936c4f5ab6bf0abaae9106f80b024171c7c9
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-03-11 12:04:58 +0600

    Update 1651B_Prove him wrong.cpp

--- Codeforces/Div 2/1651B_Prove him wrong.cpp
+++ Codeforces/Div 2/1651B_Prove him wrong.cpp
@@ -1,1 +1,14 @@
 #include<bits/stdc++.h>
+using namespace std;
+void wrong(int n){
+
+}
+int main(){
+    int t,n;
+    cin>>t;
+    while(t--){
+        cin>>n;
+        wrong(n);
+    }
+return 0;
+}
