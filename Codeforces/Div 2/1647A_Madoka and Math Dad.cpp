9fcb47a29b0fd92a5c0f694eaca0a59d57391976
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-03-12 00:45:44 +0600

    ami tu bujhte parci

--- Codeforces/Div 2/1647A_Madoka and Math Dad.cpp
+++ Codeforces/Div 2/1647A_Madoka and Math Dad.cpp
@@ -2,46 +2,8 @@
 using namespace std;
 void digit(int n)
 {
-    if(n<=2)
-    {
-        cout<<n<<endl;
-    }
-    else
-    {
-        int x = n/2;
-        int mid = n%2;
-        int diff = x-mid;
-        if(diff>1){
-            mid+=diff;
-        }
-        cout<<x<<" after add"<<mid<<endl;
-        cout<<x<<" "<<mid<<endl;
-        //vector<int>v;
-        int fina=mid+x;
-        if(x>mid){
-            for(int i=0;i<fina;i++){
-                if(i%2==0){
-                    cout<<2;
-                }
-                else{
-                    cout<<1;
-                }
-            }
-            cout<<endl;
-        }
-        else{
-            for(int i=0;i<fina;i++){
-                if(i%2==0){
-                    cout<<1;
-                }
-                else{
-                    cout<<2;
-                }
-            }
-            cout<<endl;
-        }
 
-}
+
 }
 
 int main()
