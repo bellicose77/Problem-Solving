3d50e358e2cfe76bdc5957e15c15d78b17ebe008
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-03-22 21:38:09 +0600

    basic structure

--- Codeforces/Div 2/1657B_XY Sequence.cpp
+++ Codeforces/Div 2/1657B_XY Sequence.cpp
@@ -1,6 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main(){
+long long int xy_sequence(){
+    long long int n,b,x,y;
 
+
+}
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+       long long int result= xy_sequence();
+       cout<<result<<endl;
+    }
 return 0;
 }
