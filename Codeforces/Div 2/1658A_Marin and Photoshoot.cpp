f5b2d486437ed5d3dc424990199b22d8fa002b3a
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-03-27 21:49:01 +0600

    pari na kisu

--- Codeforces/Div 2/1658A_Marin and Photoshoot.cpp
+++ Codeforces/Div 2/1658A_Marin and Photoshoot.cpp
@@ -3,10 +3,24 @@ using namespace std;
 void marin(int n){
 string s;
 cin>>s;
+int c=0;
+for(int i=0;i<n-1;i++){
+    if(s[i]==0 && s[i+1]==0){
+        c+=2;
+    }
+    else if(s[i]==0 && s[i+1]==1){
+        if(s[i+2]==1){
+            c++;
+        }
+        else {
+            c;
+        }
+    }
+}
 
 }
 int main(){
-    int t;
+    int t,n;
     cin>>t;
     while(t--){
         cin>>n;
