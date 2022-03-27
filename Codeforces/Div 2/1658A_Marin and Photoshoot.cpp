8178f047251901023f005b12adeee3746c271c68
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-03-27 22:31:06 +0600

    ekta hoy abr ekta hoy na

--- Codeforces/Div 2/1658A_Marin and Photoshoot.cpp
+++ Codeforces/Div 2/1658A_Marin and Photoshoot.cpp
@@ -1,32 +1,45 @@
 #include<bits/stdc++.h>
 using namespace std;
-void marin(int n)
-{
-    string s;
-    cin>>s;
-    int c=0;
-    for(int i=0; i<n-1; i++)
-    {
-        cout<<"something"<<endl;
-        cout<<s[i]<<endl;
-        if(s[i]=='0')
-        {
-            c+=2;
-            cout<<c<<endl;
-        }
-
+void marin(int n){
+string s;
+cin>>s;
+int c=0;
+for(int i=0;i<n-1;i++){
+    if(s[i]=='0' && s[i+1]=='0'){
+        c+=2;
+        //cout<<c<<endl;
     }
+    else if(s[i]=='0' && s[i+1]=='1'){
+        if(s[i+2]=='1'){
+            i++;
+            //cout<<c<<endl;
+        }
+        else {
+             c++;
+             i++;
+        }
+    }
+    else if(s[i]=='1' && s[i+1]=='0'){
+        if(s[i+2]=='0'){
+            c+=2;
+            i++;
+
+        }
+        else{
+
+        }
+    }
+}
+cout<<c<<endl;
 
 }
-int main()
-{
+int main(){
     int t,n;
     cin>>t;
-    while(t--)
-    {
+    while(t--){
         cin>>n;
         marin(n);
     }
 
-    return 0;
+return 0;
 }
