e1f87c70f1f78817134a835a250897f52c11b2bb
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-03-27 21:26:06 +0600

    contest 779

---
+++ Codeforces/Div 2/1658A_Marin and Photoshoot.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+void marin(int n){
+string s;
+cin>>s;
+
+}
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        cin>>n;
+        marin(n);
+    }
+
+return 0;
+}
