850c59476d31a404132e631d0efa20421b73adf9
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-03-29 00:41:27 +0600

    done

--- Codeforces/Div 2/1658A_Marin and Photoshoot.cpp
+++ Codeforces/Div 2/1658A_Marin and Photoshoot.cpp
@@ -10,25 +10,13 @@ for(int i=0;i<n-1;i++){
         //cout<<c<<endl;
     }
     else if(s[i]=='0' && s[i+1]=='1'){
-        if(s[i+2]=='1'){
-            i++;
+        if(s[i+2]=='0'){
+            c+=1;
             //cout<<c<<endl;
         }
-        else {
-             c++;
-             i++;
-        }
-    }
-    else if(s[i]=='1' && s[i+1]=='0'){
-        if(s[i+2]=='0'){
-            c+=2;
-            i++;
 
-        }
-        else{
-
-        }
     }
+
 }
 cout<<c<<endl;
 
