99bb07c914ab91983ee22cdd9633c1097e907861
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-03-29 23:44:28 +0600

    start

--- Codeforces/Div 2/1658B_Marin and anti coprime.cpp
+++ Codeforces/Div 2/1658B_Marin and anti coprime.cpp
@@ -1,6 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
+int coprime(int n){
+}
 int main(){
-
+int t;
+cin>>t;
+while(t--){
+    cin>>n;
+    int result = coprime(n);
+    cout<<result<<endl;
+}
 return 0;
 }
