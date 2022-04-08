daefd7b56a29216c0411db1028961f7f7c925969
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-04-08 21:52:57 +0600

    pointer

---
+++ Codeforces/Div 2/teswx.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int arr[]={5,6,7,5,4,9};
+int *prt = &arr[0];
+cout<<*(prt++)<<endl;
+cout<<*(++prt)<<endl;
+cout<<*prt<<endl;
+return 0;
+}
