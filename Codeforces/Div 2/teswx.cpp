55e336de66efd0dbf1c81c25437b2c19ba826d60
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-04-08 22:07:40 +0600

    output

--- Codeforces/Div 2/teswx.cpp
+++ Codeforces/Div 2/teswx.cpp
@@ -1,11 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
+void fun(const int *p)
+{
+    *p=1;
+    cout<<*p<<endl;
+}
 int main(){
+/*
 int arr[]={5,6,7,5,4,9};
 int *prt = &arr[0];
 prt=&arr[0];
 cout<<*(prt++)<<endl;
 cout<<*(++prt)<<endl;
 cout<<*prt<<endl;
+*/
+const int i=10;
+cout<<&i<<endl;
+fun(&i);
 return 0;
 }
