17348043195e1902d17fae5f2404fb6768f92f0e
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-04-08 21:44:40 +0600

    Hudai

--- Codeforces/Div 2/pointer.cpp
+++ Codeforces/Div 2/pointer.cpp
@@ -2,11 +2,16 @@
 using namespace std;
 int main()
 {
+    /*
     int x=5;
     int *ptr;
     ptr=&x;
     *ptr=6;
     cout<<*ptr<<endl;
     cout<<x<<endl;
+    */
+    int *ptr;
+    *ptr=1;
+    cout<<*ptr<<endl;
     return 0;
 }
