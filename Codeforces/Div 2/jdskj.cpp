6188779f6ac54df903b79c61a645cfe430e1f617
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-04-08 23:06:55 +0600

    adf

--- Codeforces/Div 2/jdskj.cpp
+++ Codeforces/Div 2/jdskj.cpp
@@ -1,17 +1,9 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    int x = 20, *y, *z;
-
-    // Assume address of x is 500 and
-    // integer is 4 byte size
-    y = &x;
-    cout<<*y<<endl;
-    z = y;
-    cout<<*z<<endl;
-    *(y++);
-    *(z++);
-    x++;
-    printf("x = %d, y = %d, z = %d \n", x, *y, *z);
+   const int a =10;
+   const int *p = &a;
+   *p = 5;
+   cout<<a<<endl;
 return 0;
 }
