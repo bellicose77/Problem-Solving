e42907430846fd12313f0999121ae11cb9a9eda6
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-05-22 21:01:43 +0600

    contest again

---
+++ Codeforces/Div 2/1682A_Plaindromic Indices.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+void plaindrom(string s)
+{
+
+}
+int main()
+{
+    int t,n;
+    string s;
+    cin>>t;
+    while(t--)
+    {
+        cin>>n;
+        cin>>s;
+        plaindrom(s);
+    }
+    return 0;
+}
