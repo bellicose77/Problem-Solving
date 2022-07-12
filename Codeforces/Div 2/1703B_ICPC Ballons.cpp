989248803a9f7f6228ced97640376c163a14f9b1
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-07-12 21:14:45 +0600

    khuda lagce

--- Codeforces/Div 2/1703B_ICPC Ballons.cpp
+++ Codeforces/Div 2/1703B_ICPC Ballons.cpp
@@ -1,2 +1,18 @@
 #include<bits/stdc++.h>
-
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        cin>>n;
+        string s;
+        cin>>s;
+        if(n==1){
+            cout<<2<<endl;
+        }
+        else{
+            solution(s);
+        }
+    }
+return 0;
+}
