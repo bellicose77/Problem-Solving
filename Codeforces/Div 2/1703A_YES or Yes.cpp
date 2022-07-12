7ecfe6c73b585f1a955228b082b5c558d018cce5
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-07-12 20:48:48 +0600

    staring

--- Codeforces/Div 2/1703A_YES or Yes.cpp
+++ Codeforces/Div 2/1703A_YES or Yes.cpp
@@ -1,5 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
+void solution(string s){
+
+}
 int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string s;
+        cin>>s;
+        solution(s);
+    }
 
 }
