9180fb8d01f3bfdf00d4c029b96d5829043dd3cd
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-08-02 00:04:23 +0600

    gh

--- Codeforces/Div 3/177a.cpp
+++ Codeforces/Div 3/177a.cpp
@@ -1,11 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
+int solve(int n,int h,int m)
+{
+
+}
 int main ()
 {
     int t,n,h,m;
     cin>>t;
     while(t--){
         int result = solve(n,h,m);
+        cout<<result<<endl;
     }
     return 0;
 }
