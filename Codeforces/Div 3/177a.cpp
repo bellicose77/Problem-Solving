a7f5fe5e166e532536c7dae30835ec7415298bb3
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-08-06 21:32:10 +0600

    #812

--- Codeforces/Div 3/177a.cpp
+++ Codeforces/Div 3/177a.cpp
@@ -2,6 +2,17 @@
 using namespace std;
 int solve(int n,int h,int m)
 {
+    int x,y;
+    if(h>12){
+        int H = h-24;
+    }
+    while(n--){
+        cin>>x>>y;
+        if(x>12){
+            dif = x-12;
+        }
+    }
+
 
 }
 int main ()
@@ -9,6 +20,7 @@ int main ()
     int t,n,h,m;
     cin>>t;
     while(t--){
+            cin>>n>>h>>m;
         int result = solve(n,h,m);
         cout<<result<<endl;
     }
