00196a1aa54401e67dad2c4d225718e1e30b9565
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-08-07 00:14:54 +0600

    #812

--- Codeforces/Div 2/1713A_Traveling Salesman problem.cpp
+++ Codeforces/Div 2/1713A_Traveling Salesman problem.cpp
@@ -5,9 +5,29 @@ int solve(int n)
     int x,y;
     int sx=0,sy=0;
     int ex=0,ey=0;
+    int sum=0;
+    int finalsum;
     while(n--){
         cin>>x>>y;
+        int xsum=abs((x-(sx)));
+        //cout<<"xsum:"<<xsum<<endl;
+        int ysum = abs(y-(sy));
+        //cout<<"ysum:"<<ysum<<endl;
+        int yyy = (xsum+ysum);
+        sum=sum+yyy;
+        //int finalsum=sum;
+       // cout<<"sum:loop: "<<sum<<endl;
+        sx=x;
+        sy=y;
+        //cout<<sum<<endl;
     }
+    //cout<<"sum:"<<sum<<endl;
+    int enxsum = abs(sx-(ex));
+    int enysum = abs(sy-(ey));
+    int ysum = enxsum+enysum;
+
+    int cc=sum+ysum;
+    return cc;
 }
 int main(){
     int t,n;
@@ -15,6 +35,7 @@ int main(){
     while(t--){
         cin>>n;
         int result = solve(n);
+        cout<<result<<endl;
     }
 return 0;
 }
