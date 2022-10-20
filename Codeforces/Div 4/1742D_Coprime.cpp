bbbd8977386d90519bd7ef60c10c661f382135ac
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-10-20 19:23:06 +0600

    s

--- Codeforces/Div 4/1742D_Coprime.cpp
+++ Codeforces/Div 4/1742D_Coprime.cpp
@@ -1,6 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
+void solve(int n){
+    int x;
+    vector<int>v;
+    while(n--){
+        cin>>x;
+        v.push_back(x);
+    }
+    for(auto p:v){
+        cout<<p<<" ";
+    }
+    cout<<endl;
+}
 int main(){
+    int t,n;
+    cin>>t;
+    while(t--){
+        cin>>n;
+        solve(n);
+    }
     return 0;
 }
 
